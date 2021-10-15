8cb4f71e61261572300f4434c74ad51ba60c43ad
DianaFursevich <d.fursevich@bk.ru>
2021-10-15 21:59:32 +0300

    add

---
+++ PO-7(2)/Фурсевич Диана Сергеевна/ЯП/С++/1/src/realization.cpp
@@ -0,0 +1,73 @@
+#include "class.h"
+#pragma execution_character_set("utf-8")
+
+Person::Person() //конструктор по умолчанию 
+{
+	cout << "Вызвался конструктор без параметров для объекта " << this << endl;
+
+}
+
+Person::Person(char* name, int age, int floor) // прямая инициализация переменных-члена класса
+{
+	m_name = name;
+	m_age = age;
+	m_floor = floor;
+	cout << "Вызвался конструктор с параметрами для объекта " << this << endl;
+}
+
+Person::Person(const Person &other) { //конструктор копирования
+	m_name = new char[100];
+	m_name = other.m_name;
+	m_age = other.m_age;
+	m_floor = other.m_floor;
+	cout << "Конструктор копирования " << this << endl;
+}
+
+
+Person:: ~Person() {  //деструктор
+	cout << "Вызвался деструктор для объекта " << this << endl;
+}
+
+char* Person::GETName() { //для получения значений поля имя
+	return m_name;
+}
+
+int Person::GETAge() { //для получения значений поля возраст
+	return m_age;
+}
+
+
+int Person::GETFloor() { //для получения значений поля пол
+	return m_floor;
+}
+
+void Person::SETName(char* name) { //для установки значений поля имя
+	m_name = name;
+}
+
+void Person::SETAge(int age) { //для установки значений поля возраст
+	m_age = age;
+}
+
+void Person::SETFloor(int floor) { //для установки значений поля пол
+	m_floor = floor;
+}
+
+
+void Person::SET(char* name, int age, int floor) { // инициализация всех полей объекта
+	m_name = name;
+	m_age = age;
+	m_floor = floor;
+}
+
+
+
+void Person::Show() { //просмотр полей объекта
+	cout << "Имя человека " << m_name << endl;
+	cout << "Возраст " << m_age << endl;
+	cout << "Пол " << m_floor << endl << endl;
+}
+
+void Person::View(Person a) {
+	a.Show();
+}
---
+++ PO-7(2)/Фурсевич Диана Сергеевна/ЯП/С++/1/src/ЯП_С++_1.cpp
@@ -0,0 +1,70 @@
+﻿#include "class.h"
+
+Person fun() {
+	//cout << "Вызвалась функция fun" << endl;
+	char*name = new char[100];
+	cin.getline(name, 100);
+	Person p(name, 45, 0);
+	//cout << "Конец функции fun" << endl;
+	return p;
+
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+
+	char* name = new char[100];
+	Person array[1];//использование статической памяти 
+	cin.getline(name, 100);
+	array[0].SET(name, 12, 1);
+	array[0].Show();
+	cout << endl;
+
+
+
+	Person *point = &array[0]; //указатель на объект класса
+	point->Show();// вывод данных через указатель
+
+	/*
+	point->SETFloor(0);
+	cout << "указатель: " << endl;
+	point->Show();
+	cout << "объект класса: " << endl;
+	array[0].Show();*/
+
+	Person *array_1 = new Person[2]; //использование динамической памяти
+
+	cin.getline(name, 100);
+	array_1->SET(name, 34, 2);
+	array_1[0].Show();
+	cout << endl;
+
+	cin.getline(name, 100);
+	(array_1 + 1)->SET(name, 65, 2);
+	(array_1 + 1)->View(array_1[1]); //вызов конструктора копирования, когда объект передаётся функции по значению
+	cout << endl;
+	delete[] array_1;
+
+
+	//использование указателя на компонентную функцию
+	void(Person::*pf)();//определение указателя на компоненту-функцию
+	pf = &Person::Show; //настройка указателя 
+	cin.getline(name, 100);
+	Person *example = new Person(name, 23, 2);
+	(example->*pf)(); //вывод объекта
+
+	Person example_2 = Person(*example);//вызов конструктора копирования при использовании объекта для инициализации другого объекта
+	example_2.Show();
+
+
+	Person example_3 = fun();//вызов конструктора копирования при построении временного объекта как возвращаемого значения функции
+
+	delete example;
+	delete[] name;
+
+}
+
+
